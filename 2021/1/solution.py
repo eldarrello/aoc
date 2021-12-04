def8a7d7421f198e876c3a777a4ebd0a484ceebc
Eldar Rello <eldar.rello@gmail.com>
2021-12-04 12:09:47 +0200

    Compress

--- 2021/1/solution.py
+++ 2021/1/solution.py
@@ -1,10 +1,6 @@
 s = open('input.txt').read().splitlines()
 s = [int(x) for x in s]
-part_1 = 0
-for i in range(1, len(s)):
-    if s[i] > s[i - 1]:
-        part_1 += 1
-
+print("Part 1:", sum([1 for i in range(1, len(s)) if s[i] > s[i - 1]]))
 part_2 = 0
 prev_sum = 0
 for i in range(len(s) - 2):
@@ -12,5 +8,4 @@ for i in range(len(s) - 2):
     if sum > prev_sum and prev_sum != 0:
         part_2 += 1
     prev_sum = sum
-print("Part 1:", part_1)
 print("Part 2:", part_2)
