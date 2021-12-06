8e3114e2a4d5cfcdea829a70a9007d19b5f13da9
Eldar Rello <eldar.rello@gmail.com>
2021-12-06 14:06:18 +0200

    Compress

--- 2021/6/solution.py
+++ 2021/6/solution.py
@@ -1,13 +1,8 @@
 s = open('input.txt').read().splitlines()
-fs = [int(x) for x in s[0].split(',')]
-n = [fs.count(i) for i in range(9)]
-
-for day in range(256):
-    t = n[0]
-    n = [n[i + 1] for i in range(8)]
-    n.append(t)
-    n[6] += t
-    if day == 79:
+n = [[int(x) for x in s[0].split(',')].count(i) for i in range(9)]
+for i in range(256):
+    n[(i + 7) % 9] += n[i % 9]
+    if i == 79:
         print("Part 1:", sum(n))
 print("Part 2:", sum(n))
 
