ef1500b36050a92e6f2e84a13f46d0551f0d3474
Eldar Rello <eldar.rello@gmail.com>
2021-12-07 16:34:32 +0200

    Got rid of cache, which also made it noticable slower

--- 2021/7/solution.py
+++ 2021/7/solution.py
@@ -1,7 +1,4 @@
 s = open('input.txt').read().splitlines()
 n = [int(x) for x in s[0].split(',')]
 print("Part 1:", min([sum([abs(t - i) for i in n]) for t in range(max(n))]))
-b = [sum(range(i)) for i in range(1, max(n) + 2)]
-print("Part 2:", min([sum([b[abs(t - i)] for i in n]) for t in range(max(n))]))
-
-
+print("Part 2:", min([sum([abs(t - i) * (abs(t - i) + 1) // 2 for i in n]) for t in range(max(n))]))
