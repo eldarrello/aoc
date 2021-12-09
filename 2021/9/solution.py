b0615dac5181e3b5baec5cd7eb9c29a9b950f7f5
Eldar Rello <eldar.rello@gmail.com>
2021-12-09 12:15:13 +0200

    Compress

--- 2021/9/solution.py
+++ 2021/9/solution.py
@@ -1,25 +1,17 @@
 import math
-s = open('input.txt').read().splitlines()
-map = {(y, x):int(s[y][x]) for y in range(len(s)) for x in range(len(s[y]))}
-lows = []
-for yx in map.keys():
-    for m in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
-        g = (yx[0] + m[0], yx[1] + m[1])
-        if g in map and map[g] <= map[yx]:
-            break
-    else:
-        lows.append(yx)
-print("Part 1:", sum([map[p] + 1 for p in lows]))
+def moves(yx):
+    return [(yx[0] + m[0], yx[1] + m[1]) for m in [(-1, 0), (0, -1), (0, 1), (1, 0)]]
 def eval(p):
     c = set()
     nodes = [p]
     while nodes:
         p = nodes.pop(0)
         c.add(p)
-        for m in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
-            g = (p[0] + m[0], p[1] + m[1])
-            if g in map and map[g] > map[p] and map[g] < 9:
-                nodes.append(g)
+        nodes += [m for m in moves(p) if m in map and map[m] > map[p] and map[m] < 9]
     return len(c)
+s = open('input.txt').read().splitlines()
+map = {(y, x):int(s[y][x]) for y in range(len(s)) for x in range(len(s[y]))}
+lows = [yx for yx in map.keys() if not [1 for m in moves(yx) if m in map and map[m] <= map[yx]]]
+print("Part 1:", sum([map[p] + 1 for p in lows]))
 print("Part 2:", math.prod(sorted([eval(i) for i in lows])[-3:]))
 
