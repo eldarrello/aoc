b80dde8e517f662d80848ff328117b7b39c8426d
Eldar Rello <eldar.rello@gmail.com>
2021-12-10 10:02:23 +0200

    Compress

--- 2021/10/solution.py
+++ 2021/10/solution.py
@@ -1,22 +1,18 @@
 s = open('input.txt').read().splitlines()
-scores = {')': 3, ']': 57,'}': 1197,'>': 25137}
-m = {'{': '}', '[': ']', '<': '>', '(': ')'}
 part_1 = 0
 part_2 = []
 for i in s:
     o = []
     for k in i:
-        if k in m:
+        if k in '([{<':
             o.append(k)
-        else:
-            top = o.pop()
-            if k != m[top]:
-                part_1 += scores[k]
-                break
+        elif k != {'{': '}', '[': ']', '<': '>', '(': ')'}[o.pop()]:
+            part_1 += {')': 3, ']': 57, '}': 1197, '>': 25137}[k]
+            break
     else:
         n = 0
-        for i in o[::-1]:
-            n = n * 5 + ['(', '[', '{', '<'].index(i) + 1
+        for k in o[::-1]:
+            n = n * 5 + ['(', '[', '{', '<'].index(k) + 1
         part_2.append(n)
 print("Part 1:", part_1)
 print("Part 2:", sorted(part_2)[(len(part_2) - 1) // 2])
