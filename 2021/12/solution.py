1dce82c09caeb78142cc0bdd3ca812676b7b1847
Eldar Rello <eldar.rello@gmail.com>
2021-12-12 12:12:02 +0200

    Compress

--- 2021/12/solution.py
+++ 2021/12/solution.py
@@ -1,38 +1,19 @@
-s = open('input.txt').read().splitlines()
 ps = {}
-for i in s:
-    p = i.split('-')
-    if p[0] in ps:
-        ps[p[0]] = ps[p[0]] + [p[1]]
-    else:
-        ps[p[0]] = [p[1]]
-    if p[1] in ps:
-        ps[p[1]] = ps[p[1]] + [p[0]]
-    else:
-        ps[p[1]] = [p[0]]
-caves = [i for i in ps.keys() if i[0].islower()]
+for i in open('input.txt').read().splitlines():
+    s, d = i.split('-')
+    ps[s] = ps[s] + [d] if s in ps else [d]
+    ps[d] = ps[d] + [s] if d in ps else [s]
 
 def eval(part):
     count = 0
-    nodes = [i for i in ps['start']]
-    path = ['start']
+    nodes = [('start', ['start'])]
     while nodes:
-        node = nodes.pop()
-        if node == 'back':
-            path.pop()
-            continue
-        path.append(node)
+        node, path = nodes.pop()
         if node == 'end':
             count += 1
-            path.pop()
             continue
-        nodes.append('back')
-        for next in ps[node]:
-            if next[0].islower():
-                if path.count(next) > 0:
-                    if next in ['start', 'end'] or [1 for cave in caves if path.count(cave) > part]:
-                        continue
-            nodes.append(next)
+        nodes += [(i, list(path + ([i] if i[0].islower() else []))) for i in ps[node] if not i[0].islower() or path.count(i) == 0 or
+                  (i not in ['start', 'end'] and part and len(path) == len(set(path)))]
     return count
 print("Part 1:", eval(0))
 print("Part 2:", eval(1))
