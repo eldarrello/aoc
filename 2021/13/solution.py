48d2a3db2fc81c82e0dc8c772cd2a6a5c1d2d950
Eldar Rello <eldar.rello@gmail.com>
2021-12-13 12:18:14 +0200

    Compress

--- 2021/13/solution.py
+++ 2021/13/solution.py
@@ -1,15 +1,7 @@
-m = {(int(i.split(',')[0]), int(i.split(',')[1])) for i in open('input.txt').read().splitlines() if ',' in i}
+m = {(map(int, i.split(','))) for i in open('input.txt').read().splitlines() if ',' in i}
 folds = []
-for i in open('input.txt').read().splitlines():
-    if '=' in i:
-        a, b = i.split(' ')[2].split('=')
-        for x, y in list(m):
-            m.remove((x, y))
-            m.add((2 * int(b) - x if a == 'x' and x > int(b) else x, 2 * int(b) - y if a == 'y' and y > int(b) else y))
-        folds.append(len(m))
+for a, b in [i.split(' ')[2].split('=') for i in open('input.txt').read().splitlines() if '=' in i]:
+    m = {(2 * int(b) - x if a == 'x' and x > int(b) else x, 2 * int(b) - y if a == 'y' and y > int(b) else y) for x, y in m}
+    folds.append(len(m))
 print("Part 1:", folds[0])
-for y in range(6):
-    print("Part 2:", ''.join(['x ' if (x, y) in m else '  ' for x in range(100)]))
-
-
-
+[print("Part 2:", ''.join(['x ' if (x, y) in m else '  ' for x in range(100)])) for y in range(6)]
