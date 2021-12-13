40f863c2b2a156d8283cc6252501db1b0ccfa66e
Eldar Rello <eldar.rello@gmail.com>
2021-12-13 09:00:36 +0200

    Compress

--- 2021/13/solution.py
+++ 2021/13/solution.py
@@ -1,23 +1,15 @@
-s = open('input.txt').read().splitlines()
-m = {}
+m = {(int(i.split(',')[0]), int(i.split(',')[1])) for i in open('input.txt').read().splitlines() if ',' in i}
 folds = []
-for i in s:
-    if (',') in i:
-        x, y = i.split(',')
-        m[(int(x), int(y))] = 1
-    elif '=' in i:
-        _, _, t = i.split(' ')
-        folds.append(t)
-for fold in range(len(folds)):
-    a, b = folds[fold].split('=')
-    b = int(b)
-    for x, y in list(m.keys()):
-        del m[(x, y)]
-        m[(2 * b - x if a == 'x' and x > b else x, 2 * b - y if a == 'y' and y > b else y)] = 1
-    if fold == 0:
-        print("Part 1:", len(m.keys()))
-for y in range(10):
-    print(''.join(['x ' if (x, y) in m else '  ' for x in range(100)]))
+for i in open('input.txt').read().splitlines():
+    if '=' in i:
+        a, b = i.split(' ')[2].split('=')
+        for x, y in list(m):
+            m.remove((x, y))
+            m.add((2 * int(b) - x if a == 'x' and x > int(b) else x, 2 * int(b) - y if a == 'y' and y > int(b) else y))
+        folds.append(len(m))
+print("Part 1:", folds[0])
+for y in range(6):
+    print("Part 2:", ''.join(['x ' if (x, y) in m else '  ' for x in range(100)]))
 
 
 
