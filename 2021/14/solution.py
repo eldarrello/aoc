b9753c7896868f1780ee85fdaea6be32be905505
Eldar Rello <eldar.rello@gmail.com>
2021-12-14 18:36:40 +0200

    Compress a bit more

--- 2021/14/solution.py
+++ 2021/14/solution.py
@@ -3,14 +3,13 @@ f, s = open('input.txt').read().split('\n\n')
 m = {i.split(' -> ')[0]: i.split(' -> ')[1] for i in s.splitlines()}
 def eval(n):
     pairs = defaultdict(lambda: 0)
-    for i in range(len(f) - 1):
-        pairs[f[i] + f[i + 1]] += 1
+    for i in [f[i] + f[i + 1] for i in range(len(f) - 1)]:
+        pairs[i] += 1
     for t in range(1, n + 1):
-        tt = defaultdict(lambda: 0)
-        for i in pairs.keys():
-            tt[i[0] + m[i]] += pairs[i]
-            tt[m[i] + i[1]] += pairs[i]
-        pairs = tt
+        for k, v in list(pairs.items()):
+            pairs[k] -= v
+            pairs[k[0] + m[k]] += v
+            pairs[m[k] + k[1]] += v
     g = {sum([v for k, v in pairs.items() if x == k[0]]) + (1 if x == f[-1] else 0) for x in set(f)}
     return max(g) - min(g)
 print("Part 1:", eval(10))
