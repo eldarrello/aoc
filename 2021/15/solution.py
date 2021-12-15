5d383444d4148597c301329fa95e744d95d4513b
Eldar Rello <eldar.rello@gmail.com>
2021-12-15 14:11:10 +0200

    Remove redundancy

--- 2021/15/solution.py
+++ 2021/15/solution.py
@@ -2,7 +2,7 @@ def eval(n):
     map = {(y, x): (int(open('input.txt').read().splitlines()[y % 100][x % 100]) + y // 100 + x // 100 - 1) % 9 + 1 for y in range(n) for x in range(n)}
     s = {yx: (yx[0] + yx[1]) * 9 for yx in map.keys()}
     for _ in range(10):     #10 - alternative is to break out of the loop if no changes
-        for yx in list(map.keys()):
+        for yx in map.keys():
             t = [s[move] + map[yx] for move in [(yx[0] + m[0], yx[1] + m[1]) for m in [(0, 1), (1, 0), (-1, 0), (0, -1)]] if move in map and s[move] + map[yx] < s[yx]]
             if t:
                 s[yx] = min(t)
