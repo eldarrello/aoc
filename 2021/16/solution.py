a4f93cc4628567d09e462036639899bb0ed6067f
Eldar Rello <eldar.rello@gmail.com>
2021-12-16 14:37:57 +0200

    Cosmetics

--- 2021/16/solution.py
+++ 2021/16/solution.py
@@ -6,23 +6,23 @@ total = 0
 def read(n):
     global p
     p += n
-    return bits[p - n: p]
+    return int(bits[p - n: p], 2)
 
 def read_packet():
     global total
-    total += int(read(3), 2)
-    type = int(read(3), 2)
+    total += read(3)
+    type = read(3)
     if type == 4:
-        v = ''
-        while read(1) == '1':
-            v += read(4)
-        return int(v + read(4), 2)
+        v = 0
+        while read(1) == 1:
+            v = v * 16 + read(4)
+        return v * 16 + read(4)
     else:
-        if read(1) == '0':
-            bits_in_sub = int(read(15), 2)
+        if read(1) == 0:
+            bits_in_sub = read(15)
             vs = [read_packet() for i in range(p, p + bits_in_sub) if p < i]
         else:
-            vs = [read_packet() for _ in range(int(read(11), 2))]
+            vs = [read_packet() for _ in range(read(11))]
         return [sum(vs), math.prod(vs), min(vs), max(vs), 0, vs[0] > vs[-1], vs[0] < vs[-1], vs[0] == vs[-1]][type]
 print("Part 2:", read_packet())
 print("Part 1:", total)
