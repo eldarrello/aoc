faad6e4abd420253e1800bf6278e68b2fff4fe01
Eldar Rello <eldar.rello@gmail.com>
2021-12-16 13:53:03 +0200

    Compress

--- 2021/16/solution.py
+++ 2021/16/solution.py
@@ -1,14 +1,12 @@
 import math
-s = open('input.txt').read()
-bits = ''.join([bin(int(i, 16))[2:].zfill(4) for i in s])
+bits = ''.join([bin(int(i, 16))[2:].zfill(4) for i in open('input.txt').read()])
 p = 0
 total = 0
 
 def read(n):
     global p
-    v = bits[p: p + n]
     p += n
-    return v
+    return bits[p - n: p]
 
 def read_packet():
     global total
@@ -16,38 +14,15 @@ def read_packet():
     type = int(read(3), 2)
     if type == 4:
         v = ''
-        while(True):
-            bs = read(5)
-            v += (bs[1:])
-            if bs[0] == '0':
-                break
-        return int(v, 2)
+        while read(1) == '1':
+            v += read(4)
+        return int(v + read(4), 2)
     else:
-        values = []
-        ind = read(1)
-        if ind == '0':
+        if read(1) == '0':
             bits_in_sub = int(read(15), 2)
-            start = p
-            while p < start + bits_in_sub:
-                values.append(read_packet())
+            values = [read_packet() for i in range(p, p + bits_in_sub) if p < i]
         else:
-            number_of_subs = int(read(11), 2)
-            for i in range(number_of_subs):
-                values.append(read_packet())
-        if type == 0:
-            return sum(values)
-        elif type == 1:
-            return math.prod(values)
-        elif type == 2:
-            return min(values)
-        elif type == 3:
-            return max(values)
-        elif type == 5:
-            return 1 if values[0] > values[1] else 0
-        elif type == 6:
-            return 1 if values[0] < values[1] else 0
-        elif type == 7:
-            return 1 if values[0] == values[1] else 0
-
+            values = [read_packet() for _ in range(int(read(11), 2))]
+        return [sum(values), math.prod(values), min(values), max(values), 0, 1 if values[0] > values[-1] else 0, 1 if values[0] < values[-1] else 0, 1 if values[0] == values[-1] else 0][type]
 print("Part 2:", read_packet())
 print("Part 1:", total)
