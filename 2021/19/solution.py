e5bb72b6686dddb8cbab004b223c90b3eaa04dcd
Eldar Rello <eldar.rello@gmail.com>
2021-12-20 15:46:00 +0200

    Compress

--- 2021/19/solution.py
+++ 2021/19/solution.py
@@ -1,21 +1,7 @@
 add = lambda a, b: (a[0] + b[0], a[1] + b[1], a[2] + b[2])
-oris = lambda x, y ,z: [(x, y, z), (x, z, -y), (x, -y, -z), (x, -z, y), (-x, -y, z), (-x, -z, -y), (-x, y, -z), (-x, z, y),
+oris = lambda x, y, z: [(x, y, z), (x, z, -y), (x, -y, -z), (x, -z, y), (-x, -y, z), (-x, -z, -y), (-x, y, -z), (-x, z, y),
         (y, -x, z), (y, z, x), (y, x, -z), (y, -z, -x), (-y, x, z), (-y, -z, x), (-y, -x, -z), (-y, z, -x),
         (z, y, -x), (z, x, y), (z, -y, x), (z, -x, -y), (-z, -y, -x), (-z, -x, y), (-z, y, x), (-z, x, -y)]
-s = open('input.txt').read().split('\n\n')
-ss = []
-for i in s:
-    vars = []
-    for v in range(24):
-        bs = []
-        for k in i.splitlines():
-            if ('---') in k:
-                continue
-            x, y, z = list(map(int, k.split(',')))
-            bs.append(oris(x, y, z)[v])
-        vars.append(bs)
-    ss.append(vars)
-
 def get_dif(a, b):
     for o in range(24):
         m = {}
@@ -24,29 +10,17 @@ def get_dif(a, b):
                 d = (x - x1, y - y1, z - z1)
                 m[d] = 1 if d not in m else (m[d] + 1)
                 if m[d] >= 12:
-                    orientations[b] = o
-                    return d
-scanners = range(len(s))
-pos = {0:(0, 0, 0)}
-orientations = {0: 0}
-not_found = list(scanners)
-ps = set()
-nodes = [0]
+                    return d, o
+ss = [[[oris(*list(map(int, k.split(','))))[v] for k in i.splitlines() if ('---') not in k] for v in range(24)] for i in open('input.txt').read().split('\n\n')]
+pos = {0: (0, 0, 0)}
+ps = set(ss[0][0])
+nodes = [(0, 0)]
 while nodes:
-    node = nodes.pop()
-    for next in list(not_found):
-        d = get_dif(ss[node][orientations[node]], next)
-        if d:
-            pos[next] = add(pos[node], d)
-            not_found.remove(next)
-            nodes.append(next)
-            for d in ss[next][orientations[next]]:
-                ps.add(add(pos[next], d))
+    node, o = nodes.pop()
+    for next in [i for i in range(30) if i not in pos]:
+        if d := get_dif(ss[node][o], next):
+            pos[next] = add(pos[node], d[0])
+            nodes.append((next, d[1]))
+            ps |= {add(pos[next], p) for p in ss[next][d[1]]}
 print("Part 1:", len(ps))
-max_d = 0
-for i in scanners:
-    x, y, z = pos[i]
-    for k in scanners:
-        dx, dy, dz = pos[k]
-        max_d = max(max_d, abs(x - dx) + abs(y - dy) + abs(z - dz))
-print("Part 2:", max_d)
+print("Part 2:", max([abs(pos[a][0] - pos[b][0]) + abs(pos[a][1] - pos[b][1]) + abs(pos[a][2] - pos[b][2]) for a in range(30) for b in range(30)]))
