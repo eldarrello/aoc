1eb5a093aebf1f79f540478dd55d913521331a64
Eldar Rello <eldar.rello@gmail.com>
2022-12-03 14:12:13 +0200

    compress

--- 2022/3/solution.py
+++ 2022/3/solution.py
@@ -1,7 +1,4 @@
-def get_priority(a):
-    y = ord(a)
-    return y - ord('A') + 27 if y <= ord('a') else y - ord('a') + 1
-
 s = open('input.txt').read().splitlines()
-print("Part 1:", sum([get_priority(list(set(i[:len(i) // 2]).intersection(i[len(i) // 2:]))[0]) for i in s]))
-print("Part 2:", sum([get_priority(list(set(s[i]).intersection(s[i + 1]).intersection(s[i + 2]))[0]) for i in range(0, len(s), 3)]))
+p = {chr(ord('A') + i + k): i + j for i in range(26) for k, j in ((32, 1), (0, 27))}
+print("Part 1:", sum([p[list(set(i[:len(i) // 2]).intersection(i[len(i) // 2:]))[0]] for i in s]))
+print("Part 2:", sum([p[list(set(s[i]).intersection(s[i + 1]).intersection(s[i + 2]))[0]] for i in range(0, len(s), 3)]))
