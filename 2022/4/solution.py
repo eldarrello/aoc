5608e59cae66eefcbebc5ea58f9b9875c2d39c42
Eldar Rello <eldar.rello@gmail.com>
2022-12-04 11:09:55 +0200

    Compress

--- 2022/4/solution.py
+++ 2022/4/solution.py
@@ -1,11 +1,3 @@
-s = open('input.txt').read().splitlines()
-ac1 = 0
-ac2 = 0
-for i in s:
-    a1, a2, b1, b2 = list(map(int, i.replace(',', '-').split('-')))
-    if (a1 >= b1 and a2 <= b2) or (b1 >= a1 and b2 <= a2):
-        ac1 += 1
-    if (a1 >= b1 and a1 <= b2) or (b1 >= a1 and b1 <= a2):
-        ac2 += 1
-print("Part 1:", ac1)
-print("Part 2:", ac2)
+r = [list(map(int, i.replace(',', '-').split('-'))) for i in open('input.txt').read().splitlines()]
+print("Part 1:", sum([(a1 >= b1 and a2 <= b2) or (b1 >= a1 and b2 <= a2) for a1, a2, b1, b2 in r]))
+print("Part 2:", sum([(a1 >= b1 and a1 <= b2) or (b1 >= a1 and b1 <= a2) for a1, a2, b1, b2 in r]))
