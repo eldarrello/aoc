fab68f3ad9fa8963456cb979f0dddbc5400d46dd
Eldar Rello <eldar.rello@gmail.com>
2022-12-25 11:47:16 +0200

    Correct printout

--- 2022/25/solution.py
+++ 2022/25/solution.py
@@ -1,8 +1,7 @@
 m = sum([sum([5 ** i * {'0': 0, '1': 1, '2': 2, '-': -1, '=': -2}[l[::-1][i]] for i in range(len(l))]) for l in open('input.txt').read().splitlines()])
-print("Part 1:", m)
 v = []
 while m:
     m, k = divmod(m, 5)
     v.insert(0, ['0', '1', '2', '=', '-'][k])
     m += 1 if k > 2 else 0
-print("Part 2:", ''.join(v))
+print("Part 1:", ''.join(v))
