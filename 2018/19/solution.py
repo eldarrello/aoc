ddab56b5168a120cd1fb67a1970a8de0994e7cfd
Eldar Rello <eldar.rello@gmail.com>
2023-01-02 16:26:25 +0200

    2018 - 19

--- 2018/19/solution.py
+++ 2018/19/solution.py
@@ -1,44 +1,24 @@
 s = open('input.txt').read().splitlines()
-map = ''
-w = 0
-for i in s:
-    if len(i) > w:
-        w = len(i)
-for i in s:
-    map +=i.ljust(w)
-x = map.index('|')
-y = 1
-d = (0, 1)
-path = []
-count = 1
+ip_reg = int(s[0].split('#ip ')[1])
+def calc(op, a, b, c, r):
+    r[c] = {'addr': r[a] + r[b], 'addi': r[a] + b, 'mulr': r[a] * r[b], 'muli': r[a] * b,
+            'setr': r[a], 'seti': a,
+    'gtir': 1 if a > r[b] else 0, 'gtri': 1 if r[a] > b else 0, 'gtrr': 1 if r[a] > r[b] else 0,
+    'eqir': 1 if a == r[b] else 0, 'eqri': 1 if r[a] == b else 0, 'eqrr': 1 if r[a] == r[b] else 0}[op]
+m = []
+for i in s[1:]:
+    op, a, b, c = i.split()
+    m.append((op, int(a), int(b), int(c)))
+r = [0 for i in range(20)]
 while True:
-    c = map[w * y + x]
-    if c == '+':
-        if d == (0, 1) or d == (0, -1):
-            if map[w * y + x + 1] != ' ':
-                d = (1, 0)
-            elif map[w * y + x - 1] != ' ':
-                d = (-1, 0)
-            else:
-                print('x')
-                break
-        elif d == (1, 0) or d == (-1, 0):
-            if y + 1 < 200 and map[w * (y + 1) + x] != ' ':
-                d = (0, 1)
-            elif map[w * (y - 1) + x] != ' ':
-                d = (0, -1)
-            else:
-                print('y')
-                break
-    if c.isalpha():
-        path.append(c)
-    if c == ' ':
+    ip = r[ip_reg]
+    if ip >= len(m):
         break
-    x += d[0]
-    y += d[1]
-    count += 1
-    if y < 1 or x < 1:
-        break
+    op, a, b, c = m[ip]
+    pre = str(r[:6]) + str(m[ip])
+    calc(op, a, b, c, r)
+    r[ip_reg] += 1
+print("Part 1:", r[0])
+# There is double loop in ips 2 - 15, which transforms to the following code:
+print("Part 2:", sum([x for x in range(1, 10551275 + 1) for y in range(1, 10551275 // x + 1) if x * y == 10551275]))
 
-print("Part 2:", ''.join(path))
-print("Part 2:", count)
