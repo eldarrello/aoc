368e3bb2bee63879010f92eb1d463e255f60bc59
Eldar Rello <eldar.rello@gmail.com>
2023-12-02 12:44:56 +0200

    polish 1

--- 2023/1/solution.py
+++ 2023/1/solution.py
@@ -1,16 +1,10 @@
-s = open('input.txt').read().splitlines()
 def eval(d):
     acc = 0
-    for l in s:
-        first = ''
-        for n in d:
-            l = l.replace(n, n[0] + str(d.index(n) + 1) + n[1:])
-        for i in range(len(l)):
-            if (l[i].isnumeric()):
-                if first == '':
-                    first = l[i]
-                last = l[i]
-        acc += int(first + last)
+    for l in open('input.txt').read().splitlines():
+        for i, n in enumerate(d):
+            l = l.replace(n, n[0] + str(i + 1) + n[1:])
+        ds = [c for c in l if c.isdigit()]
+        acc += int(ds[0] + ds[-1])
     return acc
 print("Part 1:", eval([]))
 print("Part 2:", eval(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']))
