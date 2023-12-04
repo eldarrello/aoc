ba8043218bb6b3eabae32393c59ed65b09353345
Eldar Rello <eldar.rello@gmail.com>
2023-12-04 09:44:38 +0200

    polish 4

--- 2023/4/solution.py
+++ 2023/4/solution.py
@@ -1,15 +1,13 @@
-acc1 = 0
-acc2 = 0
+acc = 0
 s = open('input.txt').read().splitlines()
 copies = [1 for i in s]
 for y, l in enumerate(s):
   a, b = l.split(': ')[1].split(' | ')
-  w = [int(i) for i in a.split(' ') if i.isnumeric()]
-  q = [int(i) for i in b.split(' ') if i.isnumeric()]
+  w = [int(i) for i in a.split()]
+  q = [int(i) for i in b.split()]
   n = sum([1 for i in w if i in q])
-  acc1 += int(2 ** (n - 1))
-  acc2 += copies[y]
+  acc += int(2 ** (n - 1))
   for i in range(y, y + n):
     copies[i + 1] += copies[y]
-print("Part 1:", acc1)
-print("Part 2:", acc2)
+print("Part 1:", acc)
+print("Part 2:", sum(copies))
