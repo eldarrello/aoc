f6540fe75ee109988706b640554c69cfb589cf57
Eldar Rello <eldar.rello@gmail.com>
2023-12-11 18:05:43 +0200

    10 without hacks

--- 2023/10/solution.py
+++ 2023/10/solution.py
@@ -1,30 +1,15 @@
 s = open('input.txt').read().splitlines()
 m = {(x, y): s[y][x] for y in range(len(s)) for x in range(len(s[0]))}
 x, y = [(x, y) for x, y in m.keys() if m[(x, y)] == 'S'][0]
-dx, dy = 0, 1           #hack for specific input.txt
-loop = set([(x, y)])
-while True:
-    x, y = x + dx, y + dy
+gg = [(dx, dy) for (dx, dy), v in {(1, 0): '-7J', (-1, 0): '-FL', (0, 1): '|LJ', (0, -1): '|F7'}.items() if m[(x + dx, y + dy)] in v]
+dx, dy = gg[0]
+m[(x, y)] = {(-1,0,0,1):'7', (0,-1,0,1):'|', (1,0,0,1):'F', (-1,0,0,-1):'J', (-1,0,1,0):'-', (0,-1,1,0):'L'}[gg[0] + gg[1]]
+loop = set()
+while (x, y) not in loop:
     loop.add((x, y))
-    v = m[(x, y)]
-    if v == 'S':
-        m[x, y] = '7'   #hack for specific input.txt
-        break
-    if v in 'FJ':
-        dx, dy = -dy, -dx
-    elif v in 'L7':
-        dx, dy = dy, dx
-
-def is_inside(x, y):
-    acc = 0
-    s = 0
-    for i in range(x):
-        if (i, y) in loop and m[(i, y)] != '-':
-            p, s = {'|':(1, 0), 'L':(0, 'L'), 'F':(0, 'F'), '7':(1 if s == 'L' else 0, 0), 'J':(1 if s == 'F' else 0, 0)}[m[(i, y)]]
-            acc += p
-    return acc % 2 == 1
-
+    x, y = x + dx, y + dy
+    dx, dy = {'F': (-dy, -dx), 'J': (-dy, -dx), 'L': (dy, dx), '7': (dy, dx), '-': (dx, dy), '|': (dx, dy)}[m[(x, y)]]
 print("Part 1:", len(loop) // 2)
-print("Part 2:", sum([1 for x, y in m.keys() if (x, y) not in loop and is_inside(x, y)]))
+print("Part 2:", sum([1 for (x, y) in m.keys() if (x, y) not in loop and ''.join([m[(i, y)] for i in range(x) if (i, y) in loop and m[(i, y)] != '-']).replace('L7', '|').replace('FJ', '|').count('|') % 2]))
 
 
