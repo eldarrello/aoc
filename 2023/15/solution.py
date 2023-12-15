de272cd0289cb8d820880cc0f95119353b45c0ca
Eldar Rello <eldar.rello@gmail.com>
2023-12-15 11:30:39 +0200

    15 just capture one compression idea

--- 2023/15/solution.py
+++ 2023/15/solution.py
@@ -1,9 +1,7 @@
-acc1 = 0
 boxes = [{} for i in range(256)]
 for k in open('input.txt').read().split(','):
-    v = 0
-    hashes = [v:= 17 * (v + ord(c)) % 256 for c in k]
-    acc1 += hashes[-1]
+    hashes = [v:= 17 * ((v if i else 0) + ord(c)) % 256 for i, c in enumerate(k)]
+    acc1 = hashes[-1] + (acc1 if 'acc1' in vars() else 0)
     label = ''.join([c for c in k if c.isalpha()])
     h = hashes[len(label) - 1]
     if '-' in k:
