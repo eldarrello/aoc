69974a2c001a59372739078f37e9da539802675f
Eldar Rello <eldar.rello@gmail.com>
2024-01-06 22:58:17 +0200

    Alternative solution for 19

--- 2023/19/solution.py
+++ 2023/19/solution.py
@@ -1,36 +1,36 @@
-import math
+def apply(d, cond, y = [(1, min, 0), (0, max, 0)]):
+    dd = copy.deepcopy(d)
+    y = y['<>'.index(cond[1])]
+    i = 'xmas'.index(cond[0])
+    dd[i][y[0]] = y[1](dd[i][y[0]], int(cond[2:]) + y[2])
+    return dd
+
+import math, copy
 a, b = open('input.txt').read().split('\n\n')
 f = {}
 for i in a.splitlines():
     key, r = i.split('{')
     f[key] = r[:-1].split(',')
-acc2 = 0
 acc1 = 0
-nodes = [('in', [])]
+acc2 = 0
+nodes = [('in', [[0, 4001], [0, 4001], [0, 4001], [0, 4001]])]
 while nodes:
-    key, conds = nodes.pop()
+    key, d = nodes.pop()
     for k in f[key]:
         cond = 'x>0'
         if ':' in k:
             cond, k = k.split(':')
         if k == 'A':
-            d = {'x': [1, 4000], 'm': [1, 4000], 'a': [1, 4000], 's': [1, 4000]}
-            for k in conds + [cond]:
-                for c, y in {'>=': (0, max, 0), '>': (0, max, 1), '<=': (1, min, 0), '<': (1, min, -1)}.items():
-                    if c in k:
-                        l, v = k.split(c)
-                        d[l][y[0]] = y[1](d[l][y[0]], int(v) + y[2])
-                        break
-            acc2 += math.prod([max(0, high - low + 1) for low, high in d.values()])
+            dd = apply(d, cond)
+            acc2 += math.prod([max(0, high - low - 1) for low, high in apply(d, cond)])
             for i in b.splitlines():
                 acc = 0
-                for c in i[1:-1].split(','):
-                    k, v = c.split('=')
-                    acc += int(v) if d[k][0] <= int(v) <= d[k][1] else -4 * 4000
+                for j, c in enumerate(i[1:-1].split(',')):
+                    acc += int(c[2:]) if dd[j][0] < int(c[2:]) < dd[j][1] else -4 * 4001
                 acc1 += max(0, acc)
         elif k != 'R':
-            nodes.append((k, conds  + [cond]))
-        conds.append([cond.replace(k, v) for k, v in {'>=':'<', '<=':'>', '>':'<=', '<':'>='}.items() if k in cond][0])
+            nodes.append((k, apply(d, cond)))
+        d = apply(d, cond, [(0, max, -1), (1, min, 1)])
 print("Part 1:", acc1)
 print("Part 2:", acc2)
 
