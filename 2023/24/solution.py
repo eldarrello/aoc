fe3146948d25dc0b7cbcb38a1cddf00e04f26eab
Eldar Rello <eldar.rello@gmail.com>
2024-01-06 22:47:07 +0200

    24 without 3rd party lib

--- 2023/24/solution.py
+++ 2023/24/solution.py
@@ -11,11 +11,46 @@ for (x, y, _, dx, dy, _), (x2, y2, _, dx2, dy2, _) in itertools.combinations(bs,
         if min_v <= x + t1 * dx <= max_v and min_v <= y + t1 * dy <= max_v and t1 >= 0 and t2 >= 0:
             acc += 1
 print("Part 1:", acc)
-from z3 import *
-rs = RealVector('r', 6)
-ts = RealVector('t', 3)
-s = Solver()
-s.add(*[rs[d] + rs[d+3] * t == b[d] + b[d+3] * t for t, b in zip(ts, bs) for d in range(3)])
-s.check()
-print("Part 2:", s.model().eval(sum(rs[:3])))
-#Todo::own implementation without Z3
+
+import collections
+Vec3 = collections.namedtuple("Vec3", "x,y,z", defaults = (0, 0, 0))
+
+def cross(a, b):
+    return Vec3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x)
+
+def dot(a, b):
+    return a.x * b.x + a.y * b.y + a.z * b.z
+
+def sub(a, b):
+    return Vec3(a.x - b.x, a.y - b.y, a.z - b.z)
+
+def mul(a, b):
+    return Vec3(a.x * b, a.y * b, a.z * b)
+
+def add(a, b):
+    return Vec3(a.x + b.x, a.y + b.y, a.z + b.z)
+
+'''Make h0 as a base, find plane n using h1, find intersection points p2, p3 of h2 and h3 with the plane n.'''
+a = bs[0]
+h0 = Vec3(*a[:3])
+b = list(map(int.__sub__, bs[1], a))
+v1 = Vec3(*b[3:])
+h1 = Vec3(*b[:3])
+c = list(map(int.__sub__, bs[2], a))
+v2 = Vec3(*c[3:])
+h2 = Vec3(*c[:3])
+d = list(map(int.__sub__, bs[3], a))
+v3 = Vec3(*d[3:])
+h3 = Vec3(*d[:3])
+
+n = cross(h1, add(h1, v1))
+
+t2 = dot(h2, n) / -dot(v2, n)
+p2 = add(h2, mul(v2, t2))
+
+t3 = dot(h3, n) / -dot(v3, n)
+p3 = add(h3, mul(v3, t3))
+
+r = add(add(p2, mul(sub(p2, p3), t2 / (t3 - t2))), h0)
+
+print("Part 2:", int(r.x + r.y + r.z))
