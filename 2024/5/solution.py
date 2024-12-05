0f32bcfa03a1235d0661f5c24accc76c57c15293
Eldar Rello <eldar.rello@gmail.com>
2024-12-05 12:31:17 +0200

    compress 5

--- 2024/5/solution.py
+++ 2024/5/solution.py
@@ -1,32 +1,16 @@
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n - 1):
-        swapped = False
-        for j in range(n - i - 1):
-            if arr[j + 1] in m and arr[j] in m[arr[j + 1]]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                swapped = True
-        if not swapped:
-            break
-    return arr
 a, b = open('input.txt').read().split("\n\n")
+m = {}
+for c, d in [i.split('|') for i in a.splitlines()]:
+    m[c] = [d] if c not in m else m[c] + [d]
 acc = 0
 acc2 = 0
-m = {}
-for i in a.splitlines():
-    c, d = i.split('|')
-    if c not in m:
-        m[c] = [d]
-    else:
-        m[c].append(d)
-for i in b.splitlines():
-    vs = i.split(',')
-    q = bubble_sort(list(vs))
-    if vs == q:
-        acc += int(q[len(q) // 2])
-    else:
-        acc2 += int(q[len(q) // 2])
-
+for vs in [i.split(',') for i in b.splitlines()]:
+    q = list(vs)
+    for j in [j for i in range(len(q) - 1) for j in range(len(q) - i - 1)]:
+        if q[j + 1] in m and q[j] in m[q[j + 1]]:
+            q[j], q[j + 1] = q[j + 1], q[j]
+    acc += int(q[len(q) // 2]) if vs == q else 0
+    acc2 += int(q[len(q) // 2]) if vs != q else 0
 print("Part 1:", acc)
 print("Part 2:", acc2)
 
