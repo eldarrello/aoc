ae8de9e6451028e59db266fdcb9b951105a6f451
Eldar Rello <eldar.rello@gmail.com>
2026-01-08 19:42:41 +0200

    Implement 2020-18 part 2

--- 2020/18/solution.py
+++ 2020/18/solution.py
@@ -1,31 +1,36 @@
-ss = open('input.txt').read().splitlines()
-ss = ['((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2']
-#ss = ['2 + (4 * 9)']
-def eval(i):
-    op = None
-    while i < len(s):
-        k = s[i]
-        i += 1
-        if k == '(':
-            k, i = eval(i)
-        if k == ')':
-            return r, i
-        elif k in ['+', '*']:
-            op = k
+def shunting_yard(s, p):
+    ops = []
+    rpn = []
+    for c in s:
+        if c in '+*':
+            while ops and ops[-1] != '(' and p[ops[-1]] >= p[c]:
+                rpn.append(ops.pop())
+            ops.append(c)
+        elif c == '(':
+            ops.append(c)
+        elif c == ')':
+            while ops[-1] != '(':
+                rpn.append(ops.pop())
+            ops.pop()
         else:
-            print(k)
-            k = int(k)
-            if op:
-                if op == '+':
-                    r += k
-                else:
-                    r *= k
-            else:
-                r = k
-    return r, i
+            rpn.append(int(c))
+    while ops:
+        rpn.append(ops.pop())
+    while rpn:
+        c = rpn.pop(0)
+        if c == '*':
+            ops.append(ops.pop() * ops.pop())
+        elif c == '+':
+            ops.append(ops.pop() + ops.pop())
+        else:
+            ops.append(c)
+    return ops[0]
+ss = open('input.txt').read().splitlines()
 acc = 0
+acc2 = 0
 for s in ss:
     s = s.replace(' ', '')
-    acc += eval(0)[0]
+    acc += shunting_yard(s, {'+': 0, '*': 0})
+    acc2 += shunting_yard(s, {'+': 1, '*': 0})
 print("Part 1:", acc)
-print("Part 2:", 'todo') #was done by using external code:
+print("Part 2:", acc2)
